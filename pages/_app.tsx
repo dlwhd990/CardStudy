1a3a32d633ad19394617262cbaaaac0f36ee4ac0
dlwhd990 <dlwhd990@gmail.com>
2023-01-06 13:51:09 +0900

    next-seo DEFAULT_SEO 첫 작업

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,12 +6,40 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { Provider } from "react-redux";
 import { store } from "../store/store";
+import { DefaultSeo } from "next-seo";
 config.autoAddCss = false;
+
+const DEFAULT_SEO = {
+  title: "카드 스터디",
+  description: "플래시 카드로 공부하는 곳입니다.",
+  canonical: "https://card-study.vercel.app/",
+  openGraph: {
+    type: "website",
+    locale: "ko_KR",
+    url: "https://card-study.vercel.app/",
+    title: "카카오톡, 페이스북에 링크 넣으면 올라올 타이틀",
+    site_name: "카드 스터디",
+    images: [
+      {
+        url: "https://card-study.vercel.app/images/top_banner.jpg",
+        width: 285,
+        height: 167,
+        alt: "이미지",
+      },
+    ],
+  },
+  twitter: {
+    handle: "@handle",
+    site: "@site",
+    cardType: "summary_large_image",
+  },
+};
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_API || ""}>
+        <DefaultSeo {...DEFAULT_SEO} />
         <Layout>
           <Component {...pageProps} />
         </Layout>
