e034c09629255164449776a371b7f655ae30b1e2
dlwhd990 <dlwhd990@gmail.com>
2023-01-25 16:25:15 +0900

    next-sitemap config 추가

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+const config = {
+  siteUrl: "https://card-study.vercel.app",
+  generateRobotsTxt: true,
+  exclude: ["/sitemap.xml"],
+  robotsTxtOptions: {
+    additionalSitemaps: ["https://card-study.vercel.app/sitemap.xml"],
+  },
+};
